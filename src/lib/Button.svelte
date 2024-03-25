e419d89e59e2759b2776ec41f16b667ff0a1bf7d
Gustavs Fricis Jankavs <gfjankavs@tutamail.com>
2024-03-25 16:46:25 +0200

    added icons for this project

---
+++ src/lib/Button.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+
+</script>
+
+<button class="heading--s-variant">
+    <slot name="icon"/>
+    <slot></slot>
+</button>
+
+<style>
+    button {
+        border: none;
+        border-radius: 24px;
+        padding: 16px 24px;
+        background-color: var(--color-1);
+        cursor: pointer;
+        color: #fff;
+    }
+
+    button:hover {
+        background-color: var(--color-2);
+    }
+</style>
